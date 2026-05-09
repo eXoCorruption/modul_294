ab100f94f5b654bc81765c4d5962a0d0caa6e4e3
eXoCorruption <caputi.luca@hotmail.com>
2026-05-09 11:11:02 +0200

    new file:   JS_excersises/chessboard.js

---
+++ JS_excersises/chessboard.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 8; i++) {
+  let row = "";
+  for (let j = 0; j < 8; j++) {
+    if ((i + j) % 2 === 0) {
+      row += " ";
+    } else {
+      row += "#";
+    }
+  }
+  console.log(row);
+}
