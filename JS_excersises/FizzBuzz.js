47fccf4734ab4e5afb267a9ae4fb7f01883857d4
eXoCorruption <caputi.luca@hotmail.com>
2026-05-09 11:07:55 +0200

    modified:   JS_excersises/FizzBuzz.js 	new file:   JS_excersises/loopingATriangle.js

--- JS_excersises/FizzBuzz.js
+++ JS_excersises/FizzBuzz.js
@@ -1,3 +1,11 @@
-for (let hash = "#"; hash.length <= 7; hash += "#") {
-  console.log(hash);
+for (let i = 0; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
