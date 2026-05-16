6194885b612792a70e1289fe0d278228a080d07b
eXoCorruption <caputi.luca@hotmail.com>
2026-05-16 09:51:56 +0200

    modified:   block_3/SQ3B/wiss-quiz/src/Welcome.jsx

--- block_3/SQ3B/wiss-quiz/src/Welcome.jsx
+++ block_3/SQ3B/wiss-quiz/src/Welcome.jsx
@@ -2,7 +2,7 @@ function Welcome() {
     const username = "Anna";
   return (
     <div>
-      <h2>Hallo Spieler!</h2>
+      <h2>Hallo {username}!</h2>
       <p>Bereit für die erste Frage?</p>
     </div>
   );
