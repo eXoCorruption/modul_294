1fff1f4cf5ad10bdf2cca9aecc2441c7852ca01b
eXoCorruption <caputi.luca@hotmail.com>
2026-05-16 11:27:23 +0200

    modified:   block_3/SQ3B/wiss-quiz/src/Counter.jsx

--- block_3/SQ3B/wiss-quiz/src/Counter.jsx
+++ block_3/SQ3B/wiss-quiz/src/Counter.jsx
@@ -2,35 +2,65 @@ import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [istSichtbar, setIstSichtbar] = useState(true);
+  const [titel, setTitel] = useState('Mein Counter:');
+  const [countClicks, setCountClicks] = useState(0);
 
   const erhoehen = () => {
     setCount(count + 1);
+    setCountClicks(countClicks + 1);
   };
 
   const verringern = () => {
     setCount(count - 1);
-  }
+    setCountClicks(countClicks + 1);
+    if (count < 1) {
+        setCount(0);
+    }
+
+  };
 
   const reset = () => {
     setCount(0);
-  }
+    setCountClicks(countClicks + 1);
+  };
 
   const plusZehn = () => {
     setCount(count + 10);
-  }
+    setCountClicks(countClicks + 1);
+  };
 
   const minusZehn = () => {
     setCount(count - 10);
-  }
+    setCountClicks(countClicks + 1);
+    if (count < 1) {
+        setCount(0);
+    }
+  };
+
+  const toggle = () => {
+    setIstSichtbar(!istSichtbar);
+    setCountClicks(countClicks + 1);
+  };
 
   return (
     <div>
-      <p>Aktueller Zähler: {count}</p>
-      <button onClick={erhoehen}>+1</button>
-      <button onClick={plusZehn}>+10</button>
-      <button onClick={verringern}>-1</button>
-      <button onClick={minusZehn}>-10</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={toggle}>
+        {istSichtbar ? 'Counter Verstecken' : 'Counter Anzeigen'}
+      </button>
+
+      {istSichtbar && (
+      <div>
+        <p> {titel} </p>
+        <p>Anzahl aller Buttons geklickt: {countClicks}</p>
+        <p>Aktueller Zähler: {count}</p>
+        <button onClick={erhoehen}>+1</button>
+        <button onClick={plusZehn}>+10</button>
+        <button onClick={verringern}>-1</button>
+        <button onClick={minusZehn}>-10</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+     )}
     </div>
   );
 }
