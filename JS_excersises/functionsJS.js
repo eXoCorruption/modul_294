f6adb401a93ea72717270a77de4e753bf5ee3001
eXoCorruption <caputi.luca@hotmail.com>
2026-05-22 12:49:37 +0200

    modified:   JS_excersises/functionsJS.js

--- JS_excersises/functionsJS.js
+++ JS_excersises/functionsJS.js
@@ -14,7 +14,7 @@ const roundTo = function(i, step) {
     return i - remainder + (remainder < step / 2 ? 0 : step);
 };
 
-console.log(roundTo(43,44));
+console.log(roundTo(23,10));
 
 const halve = function(n) {
     return n / 2;
@@ -24,3 +24,25 @@ let n = 10;
 console.log("Die Hälfte von 100 ist: "+halve(100));
 console.log(halve(n));
 
+const hummus = function(factor) {
+    const ingredient = function(amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1) {
+            unit += "s";
+        }
+        console.log('${ingredientAmount} ${unit} ${name}');
+    };
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+};
+
+function wrapValue(n) {
+    let local = n;
+    return () => local;
+}
+let wrap1 = wrapValue(1);
+console.log(wrap1())
