af705d5f049cbbf0cfe8391e897fb3e7073b05a1
eXoCorruption <caputi.luca@hotmail.com>
2026-05-23 09:04:40 +0200

    new file:   block_5/SQ5A/wiss-quiz/.gitignore 	new file:   block_5/SQ5A/wiss-quiz/README.md 	new file:   block_5/SQ5A/wiss-quiz/eslint.config.js 	new file:   block_5/SQ5A/wiss-quiz/index.html 	new file:   block_5/SQ5A/wiss-quiz/package-lock.json 	new file:   block_5/SQ5A/wiss-quiz/package.json 	new file:   block_5/SQ5A/wiss-quiz/public/favicon.svg 	new file:   block_5/SQ5A/wiss-quiz/public/icons.svg 	new file:   block_5/SQ5A/wiss-quiz/src/App.css 	new file:   block_5/SQ5A/wiss-quiz/src/App.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/Button.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/Counter.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/Footer.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/GameSession.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/Welcome.jsx 	new file:   block_5/SQ5A/wiss-quiz/src/assets/hero.png 	new file:   block_5/SQ5A/wiss-quiz/src/assets/react.svg 	new file:   block_5/SQ5A/wiss-quiz/src/assets/vite.svg 	new file:   block_5/SQ5A/wiss-quiz/src/index.css 	new file:   block_5/SQ5A/wiss-quiz/src/main.jsx 	new file:   block_5/SQ5A/wiss-quiz/vite.config.js

---
+++ block_5/SQ5A/wiss-quiz/src/Welcome.jsx
@@ -0,0 +1,11 @@
+function Welcome() {
+    const username = "Luca";
+  return (
+    <div>
+      <h2>Hallo {username}!</h2>
+      <p>Bereit für die erste Frage?</p>
+    </div>
+  );
+}
+
+export default Welcome;
