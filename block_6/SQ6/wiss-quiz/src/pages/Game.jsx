b456520695a5e25644ea49334c4482c3bd09b4f2
eXoCorruption <caputi.luca@hotmail.com>
2026-05-23 15:20:42 +0200

    deleted:    block_5/SQ5A/wiss-quiz/vite_api.env 	modified:   block_6/SQ6/wiss-quiz/src/pages/Game.jsx

--- block_6/SQ6/wiss-quiz/src/pages/Game.jsx
+++ block_6/SQ6/wiss-quiz/src/pages/Game.jsx
@@ -2,67 +2,26 @@ import { useState, useEffect } from "react";
 import GameSession from "../components/GameSession";
 
 const API_URL = import.meta.env.VITE_API_URL;
-console.log("API_URL:",API_URL);
-
-const questions = [
-{
-  text: "Welches Studio hat das Spiel: \"Minecraft\" entwickelt?",
-  answers: ["Valve","Mojang","Ubisoft","Rockstar Games"],
-  correctAnswer: "Mojang",
-},
-{
-  text: "Wann ist das Spiel: \"The Witcher 3\" erschienen?",
-  answers: ["2013","2015","2017","2011"],
-  correctAnswer: "2015",
-},
-{
-  text: "Welche Konsole wurde von Nintendo veröffentlicht?",
-  answers: ["PlayStation 2","Xbox 360","Nintendo Switch","Steam Deck"],
-  correctAnswer: "Nintendo Switch",
-},
-{
-  text: "Wer ist der Hauptcharakter in: \"The Legend of Zelda\"?",
-  answers: ["Zelda","Link","Ganondorf","Mario"],
-  correctAnswer: "Link",
-},
-{
-  text: "Welches Spiel gehört zum Genre Battle Royale?",
-  answers: ["FIFA 24","Fortnite","The Sims 4","Need for Speed"],
-  correctAnswer: "Fortnite",
-},
-{
-  text: "Wann ist das Spiel: \"Grand Theft Auto V\" erschienen?",
-  answers: ["2010","2013","2016","2018"],
-  correctAnswer: "2013",
-},
-{
-  text: "In welchem Spiel kämpft man gegen Creeper?",
-  answers: ["Terraria","Minecraft","Roblox","Rust"],
-  correctAnswer: "Minecraft",
-},
-{
-  text: "Welche Firma entwickelt die \"PlayStation\"?",
-  answers: ["Microsoft","Nintendo","Sony","Sega"],
-  correctAnswer: "Sony",
-},
-{
-  text: "Wie heißt der Klempner aus den Nintendo-Spielen?",
-  answers: ["Luigi","Bowser","Mario","Toad"],
-  correctAnswer: "Mario",
-},
-{
-  text: "Welches Spiel ist bekannt für den Satz: \"The cake is a lie\"?",
-  answers: ["Portal","Half-Life","BioShock","DOOM"],
-  correctAnswer: "Portal",
-},
-];
 
 
 function Game() {
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadQuestions() {
+      const response = await fetch(`${API_URL}/questions`);
+      const data = await response.json();
+      setQuestions(data);
+      setIsLoading(false);
+    }
+    loadQuestions();
+  }, []);
+
   return (
     <div>
-      <h1>Quiz</h1>
-      <GameSession questions={questions} />
+      <h2>Quiz</h2>
+      <GameSession questions={questions}/>
     </div>
   );
 }
