c90cdccd25ab2b63d1555776036848263d9cf042
eXoCorruption <caputi.luca0601@gmail.com>
2026-05-28 16:52:36 +0200

    modified:   block_6/SQ6/wiss-quiz/src/pages/Game.jsx

--- block_6/SQ6/wiss-quiz/src/pages/Game.jsx
+++ block_6/SQ6/wiss-quiz/src/pages/Game.jsx
@@ -7,17 +7,45 @@ const API_URL = import.meta.env.VITE_API_URL;
 function Game() {
   const [questions, setQuestions] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function loadQuestions() {
+     try {
       const response = await fetch(`${API_URL}/questions`);
+      if (!response.ok) {
+         throw new Error("Fragen konnten nicht geladen werden");
+      }
       const data = await response.json();
       setQuestions(data);
-      setIsLoading(false);
-    }
-    loadQuestions();
+    } catch (err) {
+      setError(err.message);
+    } finally {
+    setIsLoading(false);
+  }
+  
+   } loadQuestions();
   }, []);
 
+  if (isLoading) {
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <p>Lade Fragen...</p>
+    </div>
+  );
+}
+if (error) {
+  return (
+    <div>
+      <h2>Quiz</h2>
+      <p>Fehler: {error}</p>
+      <button onClick={() => window.location.reload()}>
+        Erneut versuchen
+      </button>
+    </div>
+  );
+}
   return (
     <div>
       <h2>Quiz</h2>
